4a12f3eaef1587882313a9ba77bd4d94b3f70f32
RanggaHijriawaHaryadi <rangghrydi04@gmail.com>
2023-04-14 15:25:59 +0700

    menambahkan varibale comparisons dengan perulangan for ketika i=0 i<n,i++

--- linesearch/linesearch.cpp
+++ linesearch/linesearch.cpp
@@ -41,4 +41,15 @@ void linearSearch()
 		int item;
 		cin >> item;
 
+		comparisons = 0;
+		for (i = 0; i < n; i++)// langkah 2, 3 dab 4
+		{
+			comparisons++;
+			if (arr[i] == item)
+			{
+				cout << "\n" << item << " found at position " << (i + 1) << endl;
+				break;
+			}
+		}
+
 		
