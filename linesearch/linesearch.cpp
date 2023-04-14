7af11ec818d4824915651e73f32f9e910fac86c5
RanggaHijriawaHaryadi <rangghrydi04@gmail.com>
2023-04-14 14:49:23 +0700

    masukkan void input  while

--- linesearch/linesearch.cpp
+++ linesearch/linesearch.cpp
@@ -5,3 +5,15 @@ int arr[20]; // Array to be searched
 int n; // Number of element in the array
 int i; // Index of array element
 
+void input()
+{
+	while (true)
+	{
+		cout << "Enter the number of eleents in the array : ";
+		cin >> n;
+		if ((n > 0) && (n <= 20))
+			break;
+		else
+			cout << "\nArray should have minimu, 1 and maximum 20 elements \n\n";
+	}
+}
